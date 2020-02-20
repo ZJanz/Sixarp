67d287c3cd48a3455ec171884405ff0c9ff7d783
Shayne Fitzgerald <shaynefitzgerald0@gmail.com>
2020-02-19 23:05:29 -0500

    finish refactor & checkout server index from master

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,77 +2,112 @@ const path = require('path');
 
 const webpack = require('webpack');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-console.log(`[webpack.config.js] Executing in mode: ${process.env.NODE_ENV || 'NOT SET'}`)
 
 const WEBPACK_DEV_SERVER_PORT = 32488;
 
-module.exports = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
-  entry: {
-    vendor: [
-      'react',
-      'react-dom',
-      'react-hot-loader/patch'
-    ],
-    app: [
-      `webpack-dev-server/client?http://localhost:${WEBPACK_DEV_SERVER_PORT}`,
-      path.resolve(__dirname, './src/js/index.js')
-    ]
-  },
-  module: {
-    rules: [
-      { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.s[ac]ss$/,
-        exclude: /node_modules/,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader'
-        ]
+module.exports = env => {
+  if (!env) {
+    env = { NODE_ENV: process.env.NODE_ENV };
+  }
+  console.log(`[webpack.config.js] Executing in mode: ${env.NODE_ENV || 'NOT SET, defaults to development'}`)
+  return {
+    mode: env.NODE_ENV === 'production' ? 'production' : 'development',
+    entry: {
+      vendor: [
+        'react',
+        'react-dom',
+        'react-hot-loader/patch'
+      ],
+      app: [
+        `webpack-dev-server/client?http://localhost:${WEBPACK_DEV_SERVER_PORT}`,
+        path.resolve(__dirname, './src/js/index.js')
+      ]
+    },
+    module: {
+      rules: [
+        { enforce: 'pre', test: /\.js$/, loader: 'source-map-loader' },
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: [
+            {
+              loader: 'babel-loader',
+              options: {
+                presets: [
+                  "@babel/preset-react",
+                  "@babel/preset-env"
+                ]
+              }
+            }
+          ]
+        },
+        {
+          test: /\.s[ac]ss$/,
+          exclude: /node_modules/,
+          use: [
+            'style-loader',
+            'css-loader',
+            'sass-loader'
+          ]
+        },
+        {
+          test: /\.bundle\.js$/,
+          use: 'bundle-loader'
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['*', '.js', '.jsx']
+    },
+    devtool: 'inline-source-map',
+    plugins: ((env) => {
+      const ret = [
+        new CopyWebpackPlugin([
+          { from: 'client/src/static', to: '.' }
+        ]),
+        new HtmlWebpackPlugin({
+          template: path.resolve(__dirname, './src/static/index.html'),
+          filename: 'index.html',
+          inject: 'body'
+        }),
+        new CleanWebpackPlugin()
+      ];
+
+      if (env !== 'production') {
+        ret.push(new webpack.NamedModulesPlugin());
+        ret.push(new webpack.HotModuleReplacementPlugin());
       }
-    ]
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx']
-  },
-  devtool: 'inline-source-map',
-  plugins: ((env) => {
-    const ret = [
-      new CopyWebpackPlugin([
-        { from: 'client/src/static', to: '.' }
-      ]),
-      new HtmlWebpackPlugin({
-        template: path.resolve(__dirname, './src/static/index.html'),
-        filename: 'index.html',
-        inject: 'body'
-      })
-    ];
 
-    if (env !== 'production') {
-      ret.push(new webpack.NamedModulesPlugin());
-      ret.push(new webpack.HotModuleReplacementPlugin());
+      return ret;
+    })(process.env.NODE_ENV),
+    output: {
+      path: path.join(__dirname, '/dist'),
+      chunkFilename: '[name].bundle.[hash].js',
+      publicPath: '/',
+      filename: '[name].[hash].js'
+    },
+    optimization: ((env) => {
+      const ret = {
+        splitChunks: {
+          chunks: 'all'
+        }
+      };
 
+      if (env === 'production') {
+        ret.minimizer = [new UglifyJsPlugin()];
+      }
+
+      return ret;
+    })(process.env.NODE_ENV),
+    devServer: {
+      contentBase: './dist',
+      compress: true,
+      hot: true,
+      port: WEBPACK_DEV_SERVER_PORT
     }
-
-    return ret;
-  })(process.env.NODE_ENV),
-  output: {
-    path: path.join(__dirname, '/dist'),
-    publicPath: '/',
-    filename: '[name].[hash].js'
-  },
-  devServer: {
-    contentBase: './dist',
-    compress: true,
-    hot: true,
-    port: WEBPACK_DEV_SERVER_PORT
   }
 };
---
+++ server/index.js
@@ -0,0 +1,86 @@
+//index.js
+
+var express = require('express');
+app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+
+var rectanglesS = [];
+var playerIDs = [];
+var playersS = [];
+
+for(var i = 0; i <= 9; i++){
+		rectanglesS.push({
+			recx : Math.floor(Math.random() * 16),
+			recy : Math.floor(Math.random() * 12)
+		})
+	}
+
+	io.on('connection', function(socket){
+  	console.log(socket.id + " connected");
+  	io.to(`${socket.id}`).emit('playerArrayPOS', playersS.length);
+  	playerIDs.push(socket.id);
+  	playersS.push({
+  		x : 320,
+  		y : 240,
+  		movedx : 0,
+  		movedy : 0,
+  		right : false,
+  		left : false,
+  		up : false,
+  		down : false
+
+  	});
+  	socket.on('movement', function(state){
+  		playersS[playerIDs.indexOf(socket.id)].right = state.rightPressed;
+  		playersS[playerIDs.indexOf(socket.id)].left = state.leftPressed;
+  		playersS[playerIDs.indexOf(socket.id)].up = state.upPressed;
+  		playersS[playerIDs.indexOf(socket.id)].down = state.downPressed;
+
+  	} )
+  	io.emit('rectangleInfo', rectanglesS);
+  	socket.on('disconnect', function(){
+    	console.log(socket.id + ' disconnected');
+	})
+
+});
+
+function move(){
+	for(i = 0; i < playersS.length; i++){
+		if (playersS[i].right === true){
+	  			playersS[i].x += 2;
+	  			playersS[i].movedx += 2;
+	  		}
+
+	  		if (playersS[i].left === true){
+	  			playersS[i].x -= 2;
+	  			playersS[i].movedx -= 2;
+
+	  		}
+
+	  		if (playersS[i].up === true){
+	  			playersS[i].y -= 2;
+	  			playersS[i].movedy -= 2;
+
+	  		}
+
+	  		if (playersS[i].down === true){
+	  			playersS[i].y += 2;
+	  			playersS[i].movedy += 2;
+
+	  		}
+	  	}
+}
+
+function ping(){
+	move();
+	io.emit('playerInfo', playersS);
+}
+
+var interval = setInterval(ping, 10);
+
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
